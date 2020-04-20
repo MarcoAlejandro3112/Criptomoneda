2d58a575f049f39de446d8c63d1826ea73cae2a0
Marco Altamirano <maaltamiranohiga@gmail.com>
2020-04-19 21:25:19 -0500

    sign transactions

--- blockchain.js
+++ blockchain.js
@@ -1,10 +1,34 @@
 const SHA265 = require('crypto-js/sha256'); //Funcion de libreria para generar hash
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Transaccion{
 	constructor(origen,destino,cantidad){
 		this.origen = origen;
 		this.destino = destino;
 		this.cantidad = cantidad;
+	}
+
+	calcularHash(){
+		return SHA265(this.origen + this.destino + this.cantidad).toString();
+	}
+	signTransaccion(signingllave){
+		if(signingllave.getPublic('hex') !== this.origen){
+			throw new Error('No puedes firmar transacciones en otras carteras');
+		}
+		const hashTx = this.calcularHash();
+		const sig = signingllave.sign(hashTx,'base64');
+		this.signature = sig.toDER('hex');
+	}
+	esValido(){
+		if(this.origen === null) return true;
+
+		if(!this.signature || this.signature.length === 0){
+			throw new Error('No hay firma en esta transaccion');
+		}
+
+		const llavePublica = ec.keyFromPublic(this.origen,'hex');
+		return llavePublica.verify(this.calcularHash(),this.signature);
 	}
 }
 
@@ -29,6 +53,17 @@ class Bloque{
 		}
 		console.log("Bloque Minado: "+ this.hash);
 	}
+
+	transaccionesValidas(){
+		
+		for(let tr of this.transacciones){
+			console.log(tr);
+			if(!(tr.esValido())){
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
 class Blockchain{
@@ -45,17 +80,24 @@ class Blockchain{
 		return this.cadena[this.cadena.length -1];
 	}
 	minarTransaccionesPendientes(recompensaDir){
+		const recompensaTr = new Transaccion(null,recompensaDir,this.recompensa);
+		this.transaccionesPendientes.push(recompensaTr);
 		let bloque = new Bloque(this.getUltimoBloque().index + 1,Date.now(),this.transaccionesPendientes,this.getUltimoBloque().hash);
 		bloque.minarBloque(this.restriccion);
 		console.log("========Bloque Minado=======");
 		this.cadena.push(bloque);
-		this.transaccionesPendientes = [
-			new Transaccion(null,recompensaDir, this.recompensa)
-		];
+		this.transaccionesPendientes = [];
 	}
 
-	crearTransaccion(Transaccion){
-		this.transaccionesPendientes.push(Transaccion);
+	agregarTransaccion(transaccion){
+		
+		if(!transaccion.origen || !transaccion.destino){
+			throw new Error('La transaccion debe tener un origen y un destino');
+		}
+		if(!transaccion.esValido()){
+			throw new Error('La transaccion no es valida');
+		}
+		this.transaccionesPendientes.push(transaccion);
 	}
 
 	getDinero(direccion){
@@ -73,7 +115,9 @@ class Blockchain{
 		for(let i = 1;i< this.cadena.length;i++){
 			let bloqueActual = this.cadena[i];
 			let previoBloque = this.cadena[i - 1];
-
+			if(!bloqueActual.transaccionesValidas()){
+				return false;
+			}
 			if(bloqueActual.hash !== bloqueActual.calcularHash()){
 				return false;
 			}
@@ -84,6 +128,6 @@ class Blockchain{
 		return true;
 	}
 }
-
+module.exports.Bloque = Bloque;
 module.exports.Blockchain = Blockchain;
 module.exports.Transaccion = Transaccion;
--- index.js
+++ index.js
@@ -13,13 +13,19 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 }); CODIGO PARA INICIAR EL SERVIDOR*/
 const { Blockchain, Transaccion} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
+const miLlave = ec.keyFromPrivate('772dcf672d039840baa294b8aaa5f6e6f288bcd5da1c2648f8e10fcbdf3d6dfa');
+const cartera = miLlave.getPublic('hex');
 
 let digiCoin = new Blockchain();
-digiCoin.crearTransaccion(new Transaccion('direccion1','direccion2',50));
-digiCoin.crearTransaccion(new Transaccion('direccion3','direccion9',500));
+const tx1 = new Transaccion(cartera,'llavePublicaDeAlguien',10);
+tx1.signTransaccion(miLlave);
+digiCoin.agregarTransaccion(tx1);
+
 console.log("Minando.....");
-digiCoin.minarTransaccionesPendientes("direccionMinero");
-console.log("Balance de minero es: " + digiCoin.getDinero("direccionMinero"));
-digiCoin.minarTransaccionesPendientes("direccionMinero");
-console.log("Balance de minero es: " + digiCoin.getDinero("direccionMinero"));
+digiCoin.minarTransaccionesPendientes(cartera);
+console.log("Balance de minero es: " + digiCoin.getDinero(cartera));
+
+console.log("Es la cadena valida???: " + digiCoin.CadenaEsValida());
